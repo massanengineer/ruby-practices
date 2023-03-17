7dfd7c89f289ae9e9215a0ee771139287bee60f7
massanengineer <matsuda4645@gmail.com>
2023-03-17 15:38:50 +0900

    マージ修正

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
+require 'optparse'
+
+def current_files
+  option = ARGV.getopts('a')
+  option['a'] ? Dir.glob('*', File::FNM_DOTMATCH) : Dir.glob('*')
+end
+
 def row_column
-  files = Dir.glob('*')
+  files = current_files
   column_count = 3
   rest_of_row_count = files.size % column_count
   if rest_of_row_count >= 1
@@ -16,9 +23,10 @@ end
 def display(rows)
   rows.each do |row|
     row.each do |file_name|
-      print file_name.to_s.ljust(10)
+      print file_name.to_s.ljust(15)
     end
     puts "\n"
   end
 end
 display(row_column.transpose)
+
