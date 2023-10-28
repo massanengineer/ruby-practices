c03eaa729b02c75f1ac73322c17ed2f619bdd99a
massanengineer <matsuda4645@gmail.com>
2023-10-28 13:23:29 +0900

    rubocopで修正

--- 07.bowling_object/game.rb
+++ 07.bowling_object/game.rb
@@ -1,9 +1,9 @@
+# frozen_string_literal: true
 
-require_relative 'frame.rb'
+require_relative 'frame'
 
 class Game
   def initialize(argument)
-    
     scores = argument.split(',')
     shots = []
     scores.each do |s|
@@ -26,29 +26,22 @@ class Game
     frames[9].flatten!
 
     @frames = frames.map { |frame| Frame.new(frame[0], frame[1], frame[2]) }
-
   end
 
   def calculate_score
- 
     point = 0
-
     @frames.each_with_index do |frame, i|
       if i == 9
-        
         point += frame.frame_score
-        
-      elsif frame.first_score == 10 # 1回目ストライク
-        
-        if @frames[i + 1].first_score == 10 && i == 8 # 次の1投目がストライクかつ9フレーム
+      elsif frame.first_score == 10
+        if @frames[i + 1].first_score == 10 && i == 8
           point += @frames[i + 1].first_score + @frames[i + 1].second_score + 10
-        elsif @frames[i + 1].first_score == 10 # 次の1投目がストライク
-
+        elsif @frames[i + 1].first_score == 10
           point += 10 + @frames[i + 2].first_score + 10
-        elsif @frames[i + 1].first_score != 10 # 次の1投目がストライク以外
+        elsif @frames[i + 1].first_score != 10
           point += @frames[i + 1].first_score + @frames[i + 1].second_score + 10
         end
-      elsif frame.frame_score == 10 # スペア
+      elsif frame.frame_score == 10
         point += @frames[i + 1].first_score + 10
       else
         point += frame.frame_score
